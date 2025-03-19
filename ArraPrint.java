0bc4e9a24c4dbf8294dca03b18b8682acb5eabbe
vaishnavi Hirulkar <vaishnavihirulkar001@gmail.com>
2025-03-19 11:52:01 +0530

    hello

---
+++ ArraPrint.java
@@ -0,0 +1,16 @@
+class ArraPrint
+{
+	public static void main(String args[])
+	{
+		int a[]=new int[5];
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+}
